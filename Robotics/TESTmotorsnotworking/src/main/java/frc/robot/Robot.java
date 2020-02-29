33ae336c90f3c0e9dba5ba325b95d3a9abb062b9
matilda3 <45373100+matilda3@users.noreply.github.com>
2020-02-29 09:18:43 -0500

    Update Robot.java

--- Robotics/TESTmotorsnotworking/src/main/java/frc/robot/Robot.java
+++ Robotics/TESTmotorsnotworking/src/main/java/frc/robot/Robot.java
@@ -17,11 +17,11 @@ import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
 
-//import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorMatchResult;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-//import edu.wpi.first.wpilibj.util.Color;
-//import frc.robot.sensors.ColourSensor;
+import edu.wpi.first.wpilibj.util.Color;
+import frc.robot.sensors.ColourSensor;
 import frc.robot.util.RobotMap;
 import frc.robot.util.Telemetry;
 
@@ -68,53 +68,86 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousPeriodic() {
+    int count = 1;
     System.out.println(RobotMap.ultrasonic.getRangeMM());
-    if(RobotMap.timer.get() < 2.0){
+    if(RobotMap.timer.get() < 2.0 && count == 1){
       RobotMap.diffDrive.arcadeDrive(-0.4, 0.0);//Drive forward for 2s
+      System.out.println("forward 1sr");
+      count++;
     }
-    else if(RobotMap.timer.get() > 2.0 && RobotMap.timer.get() < 3.44){
+    else if(RobotMap.timer.get() > 2.0 && RobotMap.timer.get() < 3.44 && count == 2){
       RobotMap.diffDrive.arcadeDrive(-0.5, -0.5);//turn ccw, 90 deg
+      System.out.println("Turn towards wall");
+      count++;
     }
-    else if(RobotMap.timer.get() > 3.44 && RobotMap.ultrasonic.getRangeMM() > 2500){
-      RobotMap.diffDrive.arcadeDrive(1.0, 0.0);//drive forwards until 250cm from wall
+    else if(RobotMap.timer.get() > 3.44 && RobotMap.ultrasonic.getRangeMM() > 2500 && count == 3){
+      RobotMap.diffDrive.arcadeDrive(0.5, 0.0);//drive forwards until 250cm from wall
+      System.out.println("Drive to wall");
+      count++;
     }
-    else if(RobotMap.ultrasonic.getRangeMM() < 2500){
-      //RobotMap.diffDrive.arcadeDrive(0.0, 0.0);
+    else if(RobotMap.ultrasonic.getRangeMM() < 2400 && count == 4){
       RobotMap.timer.reset();
       RobotMap.timer.start();
       if(RobotMap.timer.get() < 1.44){
-        RobotMap.diffDrive.arcadeDrive(-0.5, -0.5);
-      }else{
-        RobotMap.timer.reset();
-        RobotMap.timer.start();
-        if(RobotMap.timer.get() < 0.5){
-          RobotMap.diffDrive.arcadeDrive(0.5, 0.0);
-      }else{
+        RobotMap.diffDrive.arcadeDrive(-0.5, -0.5);//90 deg turn ccw
+        System.out.println("Turn to goal");
+      }
+      else if(RobotMap.timer.get() > 1.44 && RobotMap.ultrasonic.getRangeMM() > 300){
+        RobotMap.diffDrive.arcadeDrive(0.5, 0.0);
+        System.out.println("Drive to goal");
+      }
+      else{
         RobotMap.diffDrive.arcadeDrive(0.0, 0.0);
+        System.out.println("Stop and balls");
         RobotMap.rampMotor.set(ControlMode.PercentOutput, 0.5);
       }
     }
-  }
+    else{
+      RobotMap.diffDrive.arcadeDrive(0.0, 0.0);
+      RobotMap.rampMotor.set(ControlMode.PercentOutput, 0.5);
+      System.out.println("END");
+    }
   }
 
   @Override
   public void teleopInit() {
     RobotMap.intakeSpinning = false;
     RobotMap.intakeUp = true;
+    RobotMap.servo0 = true;
     Telemetry.init();
   }
 
   @Override
   public void teleopPeriodic() {
     Telemetry.update();
+    System.out.println(RobotMap.colorString);
+    RobotMap.servo1.setAngle(90);
     /*U CAN ONLY PUT ONE BUTTON PER MOTOR AND IT'S SHIT
      = NO REVERSE BUTTONS*/
 
     //button 10 - servo up
-    if(RobotMap.joystick.getRawButtonPressed(10)){
-      RobotMap.servo1.setAngle(90);
-    }else{
+    /*if(RobotMap.joystick.getRawButtonPressed(10)){
+      if(RobotMap.servo0){
+        RobotMap.servo1.setAngle(90);
+        RobotMap.servo0 = false;
+      }else{
+        RobotMap.servo1.setAngle(0);
+        RobotMap.servo0 = true;
+      }
+    }*/
+    if(RobotMap.joystick.getRawButton(10)){
       RobotMap.servo1.setAngle(0);
+    }
+    if(RobotMap.joystick.getRawButton(7)){
+      System.out.println("angle" + RobotMap.servo1.getAngle());
+    }
+
+    //button 12 - color spinner motor
+    if(RobotMap.joystick.getRawButton(12)){
+      //System.out.println("press 9");
+      RobotMap.spinner.set(ControlMode.PercentOutput, 0.2);
+    }else{
+      RobotMap.spinner.set(ControlMode.PercentOutput, 0.0);
     }
 
     //button 2 - rhys's toggle intake
